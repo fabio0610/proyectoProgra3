a24f8ebdd4b14ee28023d438c0146d4d39955694
Sebastián Cambronero <sebas_cambro@hotmail.com>
2021-05-29 15:22:57 -0600

    MVC y correccion de login

---
+++ src/Controlador/Controller.java
@@ -0,0 +1,5 @@
+package Controlador;
+import Modelo.*;
+public class Controller {
+
+}
--- src/Modelo/Main.java
+++ src/Modelo/Main.java
@@ -1,16 +1,19 @@
-package login;
+package Modelo;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import Controlador.*;
+
+import java.util.Objects;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("C:\\Users\\Sebastian\\IdeaProjects\\proyectoProgra3\\src\\Vista\\login.fxml")));
         primaryStage.setTitle("LOGIN");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
