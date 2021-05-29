2fe853acb8976eb89601f8a71b21223ea7c375dc
Sebastián Cambronero <sebas_cambro@hotmail.com>
2021-05-29 16:33:54 -0600

    LOGIN

--- src/Modelo/Main.java
+++ src/Modelo/Main.java
@@ -5,10 +5,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("sample.fxml")));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
