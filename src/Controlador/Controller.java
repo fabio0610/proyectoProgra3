f3db50f8348f3aa613abfdfbbb483d4754f0655b
Sebastián Cambronero <sebas_cambro@hotmail.com>
2021-05-29 17:31:42 -0600

    Login COn metodo button

--- src/Controlador/Controller.java
+++ src/Controlador/Controller.java
@@ -1,3 +1,31 @@
 package Controlador;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    public PasswordField clave;
+    public TextField field;
+    public Button ingresarBoton;
+    public Label bloqueo;
+    int contador=0;
+
+
+    public void Button(ActionEvent actionEvent) {
+
+        if (clave.getText().equals("1234") & field.getText().equals("pepito")){
+            System.out.println("Ingresando...");
+    }
+        else{contador++;
+            System.out.println("Datos Erroneos");
+        }
+    if(contador>=3){
+        bloqueo.setText("Cuenta Bloqueada");
+        ingresarBoton.setDisable(true);
+        contador=0;
+    }
+    }
 }
+
