af2a706b61ff85bb754f66992a771258ce1c7105
Sebastián Cambronero <sebas_cambro@hotmail.com>
2021-05-30 17:19:55 -0600

    Facturar edicion

--- src/Controlador/ControllerMenu.java
+++ src/Controlador/ControllerMenu.java
@@ -1,5 +1,8 @@
 package Controlador;
 
+import Modelo.ButterMilk;
+import Modelo.DarkRoast;
+import Modelo.HouseBlend;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.MenuButton;
@@ -14,7 +17,12 @@ public class ControllerMenu {
     public MenuButton tipoCafe;
 
     public void facturar(){
-
+        ButterMilk butterMilk;
+        HouseBlend houseBlend=new HouseBlend();
+        DarkRoast darkRoast;
+if(tipoCafe.getText().equals("House Blend           ₡1400")){
+    if(lecheAlvapor.isSelected())
+      butterMilk=new ButterMilk(houseBlend);
     }
 
-}
+}}
