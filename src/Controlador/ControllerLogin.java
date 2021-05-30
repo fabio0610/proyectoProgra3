295301ac4027fed4383bad5b182d23b26fc30cbb
Sebastián Cambronero <sebas_cambro@hotmail.com>
2021-05-30 16:25:58 -0600

    Second Stage funcionando

--- src/Controlador/ControllerLogin.java
+++ src/Controlador/ControllerLogin.java
@@ -1,11 +1,23 @@
 package Controlador;
+
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
-public class ControllerLogin {
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+public class ControllerLogin implements Initializable {
     public PasswordField clave;
     public TextField field;
     public Button ingresarBoton;
@@ -13,10 +25,16 @@ public class ControllerLogin {
     int contador=0;
 
 
-    public void Button(ActionEvent actionEvent) {
+    public void Button(ActionEvent actionEvent)  throws IOException {
 
         if (clave.getText().equals("1234") & field.getText().equals("pepito")){
-            System.out.println("Ingresando...");
+           Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../VistaPrincipal/PrincipalView.fxml")));
+            Stage stage=new Stage();
+            Scene scene=new Scene(root);
+            stage.setTitle("Orders");
+            stage.setScene(scene);
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.show();
     }
         else{contador++;
             bloqueo.setText("Datos Erroneos");
@@ -26,6 +44,11 @@ public class ControllerLogin {
         ingresarBoton.setDisable(true);
         contador=0;
     }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
 
