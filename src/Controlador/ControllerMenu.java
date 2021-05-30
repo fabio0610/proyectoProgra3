3f74eb8433b2dda3c08f8052ad9d0b8904c3871d
Sebastián Cambronero <sebas_cambro@hotmail.com>
2021-05-30 16:41:05 -0600

    ControladorMenu agregacion de variables

--- src/Controlador/ControllerMenu.java
+++ src/Controlador/ControllerMenu.java
@@ -1,7 +1,19 @@
 package Controlador;
 
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.MenuButton;
 
 public class ControllerMenu {
     public Button generarFactura;
+    public CheckBox lecheAlvapor;
+    public CheckBox lecheBatida;
+    public CheckBox moca;
+    public CheckBox soya;
+    public Button nuevaOrden;
+    public MenuButton tipoCafe;
+
+    public void facturar(){
+
+    }
 }
