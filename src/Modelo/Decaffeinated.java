eca9b3e55eacb8d77411f24f45a104431a6e63af
Sebastián Cambronero <sebas_cambro@hotmail.com>
2021-06-01 15:30:58 -0600

    Atributos en caffes bases

--- src/Modelo/Decaffeinated.java
+++ src/Modelo/Decaffeinated.java
@@ -1,18 +1,25 @@
 package Modelo;
 
 public class Decaffeinated extends AbstractCoffee {
+    public Decaffeinated() {
+        tipo="Decaffeinated\n";
+        precio=1100;
+    }
+
     @Override
     public void addDecorator(Decorator decorator) {
+        precio=precio+decorator.getPrecio();
+        tipo=tipo+decorator.getTipo();
 
     }
 
     @Override
     public double getPrecio() {
-        return 1100;
+        return precio;
     }
     @Override
     public String getTipo() {
-        return "Decaffeinated\n";
+        return tipo;
     }
 
 }
